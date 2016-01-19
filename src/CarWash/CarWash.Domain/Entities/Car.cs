f02153a5efb5abbdcc965acd5c2a60d636bc4494
pedrobenevides86 <pedrobenevidesb@gmail.com>
2016-01-18 21:23:05 -0300

    Creating Domain Layer

---
+++ src/CarWash/CarWash.Domain/Entities/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace CarWash.Domain.Entities
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string LicensePlate { get; set; }
+        public Client Client { get; set; }
+    }
+}
---
+++ src/CarWash/CarWash.Domain/Entities/Client.cs
@@ -0,0 +1,8 @@
+﻿namespace CarWash.Domain.Entities
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public Car Car { get; set; } 
+    }
+}
